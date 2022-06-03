b8d411c39d4b58342447416c642c53d4098af4b0
ilobilo <ilia.chachanidze35@gmail.com>
2022-06-03 14:12:49 +0400

    fixed stdio.h

--- source/libc/stdio.h
+++ source/libc/stdio.h
@@ -2,13 +2,19 @@
 
 #pragma once
 
-#define PRINTF_ALIAS_STANDARD_FUNCTION_NAMES 1
-#include <printf/printf.h>
+#include <stddef.h>
+#include <stdarg.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+int printf(const char *format, ...);
+int vprintf(const char *format, va_list arg);
+int sprintf(char *str, const char *format, ...);
+int vsprintf(char *str, const char *format, va_list arg);
+int snprintf(char *str, size_t count, const char *format, ...);
+int vsnprintf(char *str, size_t count, const char *format, va_list arg);
 int vasprintf(char **str, const char *format, va_list args);
 int asprintf(char **str, const char *format, ...);
 
