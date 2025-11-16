9aad1bcd1b634ba576006ed0a79438cb4a766194
ilobilo <68286835+ilobilo@users.noreply.github.com>
2025-11-16 19:49:18 +0400

    update example module

--- modules/noarch/example/main.cpp
+++ modules/noarch/example/main.cpp
@@ -2,14 +2,17 @@
 
 import lib;
 
-__attribute__((constructor))
-void func() { log::info("YAYAYAYAYAYAYAYAYYAYAYAY!"); }
+namespace external
+{
+    __attribute__((constructor))
+    void func() { log::info("YAYAYAYAYAYAYAYAYYAYAYAY!"); }
 
-bool init() { log::info("Hello, World!"); return true; }
-bool fini() { log::info("Goodbye, World!"); return true; }
+    bool init() { log::info("Hello, World!"); return true; }
+    bool fini() { log::info("Goodbye, World!"); return true; }
+} // namespace external
 
 define_module {
     "example", "an example module demonstrating blah blah blah description goes here",
-    mod::generic { .init = init, .fini = fini },
+    mod::generic { .init = external::init, .fini = external::fini },
     mod::deps { "test1", "test2" }
 };
